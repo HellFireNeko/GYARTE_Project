e17203103838b0df7a0cd66a61844c4834ea01f5
HellFireDragon <johntheojacob@gmail.com>
2021-08-30 14:42:49 +0200

    Updating

---
+++ Assets/Scripts/DiscordTestBehavior.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Discord;
+
+public class DiscordTestBehavior : MonoBehaviour
+{
+    Discord.Discord discord;
+    LobbyManager lobbyManager;
+    ActivityManager activityManager;
+    long lobbyid;
+    // Use this for initialization
+    void Start()
+    {
+        var clientID = Environment.GetEnvironmentVariable("DISCORD_CLIENT_ID");
+        if (clientID == null)
+        {
+            clientID = "881860326096785409";
+        }
+        discord = new Discord.Discord(Int64.Parse(clientID), (UInt64)CreateFlags.Default);
+        discord.SetLogHook(LogLevel.Debug, LogProblemsFunction);
+
+        activityManager = discord.GetActivityManager();
+        lobbyManager = discord.GetLobbyManager();
+        var txn = lobbyManager.GetLobbyCreateTransaction();
+        txn.SetCapacity(6);
+        txn.SetType(LobbyType.Private);
+
+        lobbyManager.CreateLobby(txn, (Result result, ref Lobby lobby) =>
+        {
+
+            if (result != Result.Ok)
+            {
+                return;
+            }
+            print($"New lobby made {lobby.Id} secret {lobby.Secret}");
+
+            lobbyid = lobby.Id;
+
+            var activity = new Activity
+            {
+                ApplicationId = 881860326096785409,
+                State = "Waiting for players",
+                Details = "Co-op",
+                Party =
+            {
+                Id = lobby.Id.ToString(),
+                Size =
+                {
+                    MaxSize = 6,
+                    CurrentSize = lobbyManager.MemberCount(lobby.Id)
+                }
+            },
+                Secrets =
+            {
+                Join = lobbyManager.GetLobbyActivitySecret(lobby.Id)
+            },
+                Instance = true
+            };
+
+            activityManager.UpdateActivity(activity, result =>
+            {
+                print($"Update Activity {result}");
+            });
+        });
+    }
+
+    void OnDestroy()
+    {
+        activityManager.ClearActivity(result => { });
+        lobbyManager.DeleteLobby(lobbyid, result => { });
+        discord.Dispose();
+    }
+
+    public void LogProblemsFunction(LogLevel level, string message)
+    {
+        print($"Discord:{level} - {message}");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        discord.RunCallbacks();
+    }
+}
--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class HealthScript : MonoBehaviour
 {
+    public int StartingHealth = 3;
+
     // Start is called before the first frame update
     void Start()
     {
